152dd73e96c0e4b09b3cf89157354ac5940fe936
Yves Lorenzo <112036314+ylqgodoy@users.noreply.github.com>
2024-08-14 23:26:36 -0300

    Delete src directory

--- src/lib/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "SPPA", "sppatestdb", "SPPA");
-    if (!$conn) {
-        die("Não foi possivel conectar ao bd!");
-    }
-    date_default_timezone_set('Brazil/East');
-    mysqli_query($conn, "SET NAMES 'utf8'");
-?>
